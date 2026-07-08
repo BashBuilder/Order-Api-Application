ea2744d1a858c86315dc3a2f835131e293c3d3b2
Bashbuilder <iamadelowotan@gmail.com>
2026-07-08 07:49:15 +0100

    feat: project initialization updated

---
+++ OrderApi.Presentation/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
+app.Run();
